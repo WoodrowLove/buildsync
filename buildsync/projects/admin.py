4ef25227f2e9f8cbe63587f0ab56ad54f2a39983
WoodrowLove <woodrow227@icloud.com>
2025-02-19 20:08:14 -0500

    finished 'projects' portion of project.

--- buildsync/projects/admin.py
+++ buildsync/projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'status', 'budget', 'manager', 'start_date', 'end_date')
+    list_filter = ('status', 'manager')
+    search_fields = ('name', 'location')
